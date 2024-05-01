15e09aa393c95537dc0d57e04a15a194dbe63fb9
Lut99 <mullertim99@gmail.com>
2024-05-01 20:10:00 +0200

    Created main boilerplate structure.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # gui-rs
-A simple (I hope lol) GUI engine written for egui, with winit and wgpu.
+A simple (I hope lol) GUI engine written for [egui](https://github.com/emilk/egui), with [winit](https://github.com/rust-windowing/winit) and [wgpu](https://github.com/gfx-rs/wgpu).
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+//  LIB.rs
+//    by Lut99
+//
+//  Created:
+//    01 May 2024, 20:09:33
+//  Last edited:
+//    01 May 2024, 20:09:37
+//  Auto updated?
+//    Yes
+//
+//  Description:
+//!   A simple (I hope lol) GUI engine written for
+//!   [egui](https://github.com/emilk/egui), with
+//!   [winit](https://github.com/rust-windowing/winit) and
+//!   [wgpu](https://github.com/gfx-rs/wgpu).
+//
+
+// Declare modules
